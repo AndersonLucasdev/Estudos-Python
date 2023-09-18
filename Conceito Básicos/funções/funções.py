ba85c57032de1a93a48c207e8fbc0891246d642b
anderson <andinlucas8@gmail.com>
2023-09-18 19:55:01 -0300

    FEAT/ começando exercicos função

---
+++ Conceito Básicos/funções/funções.py
@@ -0,0 +1,17 @@
+#Calculadora de Fatorial/Binomial
+# N = 7
+# P = 5
+# def fatorial(num):
+#     soma = 1
+#     for x in range(1, num + 1):
+#         soma = soma * x
+#     return soma
+
+# def comb(N, P):
+#     res = fatorial(N) / (fatorial(P) * fatorial(N - P))
+#     return res
+
+# print(comb(N, P))
+
+
+
