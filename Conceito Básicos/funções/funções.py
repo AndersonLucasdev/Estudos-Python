c225a2a38c9279c41e150d67453b6f8597e3f403
anderson <andinlucas8@gmail.com>
2023-09-18 20:02:54 -0300

    FEAT/ fazendo os exercicios de função

--- Conceito Básicos/funções/funções.py
+++ Conceito Básicos/funções/funções.py
@@ -1,7 +1,7 @@
 #Calculadora de Fatorial/Binomial
 # N = 7
 # P = 5
-# def fatorial(num):
+# def fatorial(num: int) -> int:
 #     soma = 1
 #     for x in range(1, num + 1):
 #         soma = soma * x
@@ -13,5 +13,35 @@
 
 # print(comb(N, P))
 
+
+## retorna digitos
+# num = 12345
+# def retorna_digitos(num: int) -> int:
+#     digitos = 0
+#     aux = num
+#     while aux > 0:
+#         digitos += 1
+#         aux //= 10
+#     return digitos
+# print(retorna_digitos(num))
+
+
+## fibonnaci - até numero
+# num = 10
+
+# def fibonnaci(num: int) -> None:
+#     aux = 1
+#     aux_2 = 0
+#     while aux <= num:
+#         aux = aux_2 + aux
+#         if aux >= num:
+#             break
+#         print(aux)
+#         aux_2 = aux + aux_2
+#         if aux_2 >= num:
+#             break
+#         print(aux_2)
+
+# print(fibonnaci(num))
 
 
