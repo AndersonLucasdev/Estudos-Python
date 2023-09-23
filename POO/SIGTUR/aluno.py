c1105cb299724ec690198aecc23e6f17a7885d8a
anderson <andinlucas8@gmail.com>
2023-09-23 18:59:21 -0300

    FEAT/ começando estudos com projeto usando POO

---
+++ POO/SIGTUR/aluno.py
@@ -0,0 +1,7 @@
+class Aluno:
+    def __init__(self, id, nome):
+        self.id = 0
+        self.nome = nome
+    
+    
+    
---
+++ POO/SIGTUR/main.py
@@ -0,0 +1,7 @@
+## copia do projeto SIGTUR com POO
+def main():
+    
+    pass
+if __name__ == "__main__":
+    main()
+    
