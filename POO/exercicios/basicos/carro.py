964d1ef5cf6c0a65f7de5ef71e7e464caed0f9f0
anderson <andinlucas8@gmail.com>
2023-10-20 18:21:10 -0300

    FEAT/ novos exercícios com orientação a objetos

--- POO/exercicios/basicos/carro.py
+++ POO/exercicios/basicos/carro.py
@@ -1,20 +1,22 @@
-class Car():
-    def __init__(self, nome: str, velocidade: int):
-        self.nome = nome
-        self.velocidade = velocidade
+# class Car():
+#     def __init__(self, nome: str, velocidade: int):
+#         self.nome = nome
+#         self.velocidade = 0
     
-    def mostrar_tudo(self):
-        print(self.nome, self.velocidade)
+#     def mostrar_tudo(self):
+#         print(self.nome, self.velocidade)
     
-    def acelerar(self, incremento):
-        self.velocidade += incremento
+#     def acelerar(self, incremento):
+#         self.velocidade += incremento
     
-    def freiar(self, incremento):
-        self.velocidade -= incremento
+#     def freiar(self, incremento):
+#         self.velocidade -= incremento
 
-carro = Car('corolla', 40)
-carro.mostrar_tudo()
-carro.freiar(10)
-carro.mostrar_tudo()
-carro.acelerar(20)
-carro.mostrar_tudo()
+# carro = Car('corolla', 0)
+# carro.mostrar_tudo()
+# carro.freiar(10)
+# carro.mostrar_tudo()
+# carro.acelerar(20)
+# carro.mostrar_tudo()
+
+
