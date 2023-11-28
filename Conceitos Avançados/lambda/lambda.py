1a576a61ac24c3cc7a490ec8f3417b769c04664a
anderson <andinlucas8@gmail.com>
2023-11-28 19:01:10 -0300

    FEAT/ lambda: introduzindo novos conceitos

---
+++ Conceitos Avançados/lambda/lambda.py
@@ -0,0 +1,33 @@
+## verificar numero par
+
+# verificar_par = lambda num: True if num % 2 == 0 else False
+
+# resultado = verificar_par(8)
+# print(resultado)
+
+## ordenar lista
+# lista = [3, 1, 2, 100, -1]
+
+# ordenar_lista = lambda list: sorted(list)
+
+# lista_ordem = ordenar_lista(lista)
+# print(lista_ordem)
+
+## filtar pares
+# lista = [1, 2, 3, 4, 6, 5]
+
+# filtrar_lista = lambda lista: list(filter(lambda num: num % 2 == 0, lista))
+
+# lista_filtrada = filtrar_lista(lista)
+# print(lista_filtrada)
+
+## elevar numeros da lista
+
+# lista_num = [1, 2, 3]
+
+# elevar_num = lambda lista: list(map(lambda x: x ** 2, lista))
+
+# numeros_elevados = elevar_num(lista_num)
+# print(numeros_elevados)
+
+## 
